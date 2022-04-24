cfed148a953ece0126fead7c175079fdd61cc2b0
Anastasiya Litvinenka <loffy@rambler.ru>
2022-04-24 23:15:43 +0300

    refactor: errorservice

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,67 +1,64 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError, retry } from "rxjs/operators";
 
-import { Cards } from '../data/cards';
-import { ICard } from '../interfaces/card';
-import { ErrorService } from './error.service';
+import { Cards } from "../data/cards";
+import { ICard } from "../interfaces/card";
+import { ErrorService } from "./error.service";
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: "root",
 })
 export class CardService {
-  urlAPI = 'https://625e7a57873d6798e2a821ec.mockapi.io';
-  posts: any;
-  cards: ICard[] = Cards;
-  currentId: number | string = '';
+    urlAPI = "https://625e7a57873d6798e2a821ec.mockapi.io";
+    posts: any;
+    cards: ICard[] = Cards;
+    currentId: number | string = "";
 
-  constructor(private http: HttpClient, private errorService: ErrorService) {}
+    constructor(private http: HttpClient) // private errorService: ErrorService
+    {}
 
-  httpOptions = {
-    responseType: 'json' as const,
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-    }),
-  };
+    httpOptions = {
+        responseType: "json" as const,
+        headers: new HttpHeaders({
+            "Content-Type": "application/json",
+        }),
+    };
 
-  addId(): string {
-    return (this.currentId = new Date().getTime().toString());
-  }
+    addId(): string {
+        return (this.currentId = new Date().getTime().toString());
+    }
 
-  addCard(card: ICard): Observable<ICard> {
-    this.addId();
+    addCard(card: ICard): Observable<ICard> {
+        this.addId();
 
-    return this.http.post<ICard>(
-      this.urlAPI + '/cards',
-      card,
-      this.httpOptions
-    );
-    // .pipe(  retry(3),
-    // catchError(this.errorService.handleError('addCard', card)));
-  }
+        return this.http.post<ICard>(this.urlAPI + "/cards", card, this.httpOptions);
+        // .pipe(  retry(3),
+        // catchError(this.errorService.handleError('addCard', card)));
+    }
 
-  getAllCards(): Observable<ICard[]> {
-    return this.http.get<ICard[]>(this.urlAPI + '/cards');
-  }
+    getAllCards(): Observable<ICard[]> {
+        return this.http.get<ICard[]>(this.urlAPI + "/cards");
+    }
 
-  getCardById(id: number | string): Observable<ICard> {
-    return this.http.get<ICard>(this.urlAPI + '/cards/' + id);
-  }
+    getCardById(id: number | string): Observable<ICard> {
+        return this.http.get<ICard>(this.urlAPI + "/cards/" + id);
+    }
 
-  deleteById(id: number | string): Observable<unknown> {
-    console.log(id);
-    console.log(this.urlAPI + '/cards/' + id);
-    return this.http.delete(this.urlAPI + '/cards/' + id, {
-      params: { id: id },
-    });
-    // .pipe(catchError(this.errorService.handleError('deleteCard')));
-  }
+    deleteById(id: number | string): Observable<unknown> {
+        console.log(id);
+        console.log(this.urlAPI + "/cards/" + id);
+        return this.http.delete(this.urlAPI + "/cards/" + id, {
+            params: { id: id },
+        });
+        // .pipe(catchError(this.errorService.handleError('deleteCard')));
+    }
 
-  updateCard(card: ICard): Observable<ICard> {
-    return this.http.put<ICard>(this.urlAPI + '/cards', card, this.httpOptions);
-    // .pipe(
-    //   catchError(this.errorService.handleError(card))
-    // );
-  }
+    updateCard(card: ICard): Observable<ICard> {
+        return this.http.put<ICard>(this.urlAPI + "/cards", card, this.httpOptions);
+        // .pipe(
+        //   catchError(this.errorService.handleError(card))
+        // );
+    }
 }
